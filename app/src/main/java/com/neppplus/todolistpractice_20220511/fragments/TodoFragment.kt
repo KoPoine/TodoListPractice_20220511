9ae442ddba0d0595220d929227cbad0652d04b60
nepp.plus <nepp.plus@gamil.com>
2022-05-11 14:41:01 +0900

    ListView   - 사용할 한칸 xml을 작성완료   - 사용할 어댑터 뼈대 작업 완료   - xml의 리스트뷰에 어댑터 연결(실제 데이터 표시 X)

--- app/src/main/java/com/neppplus/todolistpractice_20220511/fragments/TodoFragment.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220511/fragments/TodoFragment.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.todolistpractice_20220511.R
+import com.neppplus.todolistpractice_20220511.adapters.TodoListAdapter
+import com.neppplus.todolistpractice_20220511.models.TodoData
+import kotlinx.android.synthetic.main.fragment_todo.*
 
 class TodoFragment: Fragment() {
+
+    val todoList = ArrayList<TodoData>()
+
+    lateinit var mTodoAdapter : TodoListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +36,17 @@ class TodoFragment: Fragment() {
     }
 
     fun setValues() {
+        todoList.add( TodoData(4.5, "오늘 수업 수강하기", "neppPlus 학원", false) )
+        todoList.add( TodoData(4.0, "오늘 수업 복습하기", "집", false) )
+        todoList.add( TodoData(3.5, "오늘 수업 출석하기", "neppPlus 학원", true) )
+        todoList.add( TodoData(3.0, "오늘 수업 수강하기", "neppPlus 학원", false) )
+        todoList.add( TodoData(2.5, "오늘 수업 수강하기", "neppPlus 학원", false) )
 
+        mTodoAdapter = TodoListAdapter(requireContext(), R.layout.todo_list_item, todoList)
+        todoListView.adapter = mTodoAdapter
+        
+        todoListView.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(requireContext(), "${i}번째 리스트입니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
