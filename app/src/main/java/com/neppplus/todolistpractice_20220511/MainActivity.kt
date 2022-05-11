c8c52f4b1efc7c5f0cd31c64d661764b2600cb05
nepp.plus <nepp.plus@gamil.com>
2022-05-11 13:14:48 +0900

    ViewPager   - 연결완료

--- app/src/main/java/com/neppplus/todolistpractice_20220511/MainActivity.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220511/MainActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.todolistpractice_20220511
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.todolistpractice_20220511.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +19,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun setValues() {}
+    fun setValues() {
+        mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mPagerAdapter
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+    }
 }
