49b2d7840fbc83af26120d1dd4540aa16c69d04f
nepp.plus <nepp.plus@gamil.com>
2022-05-11 13:05:28 +0900

    ViewPager를 위한 Fragment 3개를 생성( 화면: xml + 클래스 : kt )

---
+++ app/src/main/java/com/neppplus/todolistpractice_20220511/fragments/TodoFragment.kt
@@ -0,0 +1,33 @@
+package com.neppplus.todolistpractice_20220511.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.todolistpractice_20220511.R
+
+class TodoFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+}
