ca4c5ee64766918751dee921b531df716cdef485
nepp.plus <nepp.plus@gamil.com>
2022-05-11 12:49:09 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/todolistpractice_20220511/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.neppplus.todolistpractice_20220511
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {}
+}
